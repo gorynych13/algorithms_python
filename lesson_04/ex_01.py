291a088daf58454375be3e4b7255d5910cec5167
gorynych13 <polls@bk.ru>
2018-11-07 23:00:16 +0300

    Задание 01 четвертого урока

--- lesson_04/ex_01.py
+++ lesson_04/ex_01.py
@@ -5,7 +5,7 @@
 # Количество элементов (n) вводится с клавиатуры.
 
 # import timeit
-# import cProfile
+import cProfile
 
 #n = 10
 
@@ -16,6 +16,18 @@ def foo(n):
     return foo(n - 1) * (-0.5) + 1
 
 
+# cProfile.run('foo(10)')     10/1    0.000    0.000    0.000    0.000 ex_01.py:13(foo)
+# cProfile.run('foo(100)')    100/1    0.000    0.000    0.000    0.000 ex_01.py:13(foo)
+# cProfile.run('foo(300)')    300/1    0.001    0.000    0.001    0.001 ex_01.py:13(foo)
+
+# "ex_01.foo(10)"
+# 1000 loops, best of 5: 1.9 usec per loop
+# "ex_01.foo(20)"
+# 1000 loops, best of 5: 3.9 usec per loop
+# "ex_01.foo(100)"
+# 1000 loops, best of 5: 19.8 usec per loop
+
+#
 # def f(n):
 #     item = 1
 #     summ = 0
@@ -24,5 +36,15 @@ def foo(n):
 #         item /= -2
 #     return summ
 
+# "ex_01.f(10)"
+# 1000 loops, best of 5: 1.24 usec per loop
+# "ex_01.f(20)"
+# 1000 loops, best of 5: 2.01 usec per loop
+# "ex_01.f(100)"
+# 1000 loops, best of 5: 7.96 usec per loop
 
+
+# cProfile.run('f(10)')        1    0.000    0.000    0.000    0.000 ex_01.py:26(f)
+# cProfile.run('f(100)')        1    0.000    0.000    0.000    0.000 ex_01.py:26(f)
+# cProfile.run('f(10000)')        1    0.001    0.001    0.001    0.001 ex_01.py:26(f)
 
